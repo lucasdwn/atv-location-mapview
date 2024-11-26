cee43c9009b10e122d504491831a1e5a9dee3bf3
lucasdwn <lfcosta0804@gmail.com>
2024-11-25 22:09:52 -0300

    Atividade 5 - Dispositivos Moveis

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,26 +3,36 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../../App';
+import { useScreenGuard } from '../../hooks/useScreenGuard';
 
 type HomeScreenNavigationProp = DrawerNavigationProp<RootStackParamList, 'Home'>;
 
 const HomeScreen: React.FC = () => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
 
+  const authenticate = useScreenGuard();
+
+  const handlePress = async () => {
+    const isAuthenticated = await authenticate(); // Call biometric authentication
+    if (isAuthenticated) {
+      navigation.navigate("Screens"); // Navigate to Contacts if authenticated
+    }
+  };
+
   useEffect(() => {
     const timer = setTimeout(() => {
       navigation.navigate('Screens');
-    }, 10000); 
+    }, 10000);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, [navigation]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bem-vindo ao App!</Text>
-      <Button 
+      <Button
         title="Iniciar"
-        onPress={() => navigation.navigate('Screens')} 
+        onPress={handlePress}
       />
     </View>
   );
